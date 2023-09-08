1761b84e0b92caedb93f8b40875e92ac59df1fa6
Lukas Deutz <github@deutz.io>
2023-09-08 09:53:31 -0400

    Add console-connect function

--- zsh/dot-zsh.d/aws.zsh
+++ zsh/dot-zsh.d/aws.zsh
@@ -11,3 +11,13 @@ alias unar="awsume --unset"
 alias aws-get-id='aws sts get-caller-identity | jq .'
 alias aws-whoami='aws sts get-caller-identity | jq .'
 
+function console-connect {
+  if [[ -z "${1}" ]]; then
+    echo "Usage: console-connect production|staging"
+    return 1
+  fi
+  local -r env="${1}"
+  local -r task_id="$(aws ecs list-tasks --cluster "${env}" --service-name nbuild-console | jq -r '.taskArns[0]' | rev | cut -d/ -f1 | rev)"
+  aws ecs execute-command --cluster "${env}" --task "${task_id}" --container nbuild-console --interactive --command '/bin/sh'
+}
+
